d971625dab76ee4b4cfecdecad97f17a708dc80b
SiddharthaAnand <siddharthalibra13@gmail.com>
2020-04-11 16:12:27 +0530

    Setup config for environment differentiation

--- main_app.py
+++ main_app.py
@@ -1,8 +1,17 @@
+import sys
 from flask import Flask
+from configmodule import development_config
+from configmodule import production_config
 
+sys.path.insert(0, 'configmodule/')
 app = Flask(__name__)
 
 
 @app.route('/')
 def root_method():
     return 'Hello, Beautiful!'
+
+
+if __name__ == '__main__':
+    app.config.from_object('production_config.ProductionConfig')
+    app.run()
