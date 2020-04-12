1966e932b5241e4453e608209edad11cbc129fae
SiddharthaAnand <siddharthalibra13@gmail.com>
2020-04-12 21:15:53 +0530

    Update method according to the changes and redirect to the html page

--- main_app.py
+++ main_app.py
@@ -1,17 +1,25 @@
 import sys
-from flask import Flask
+from flask import Flask, render_template, flash
+from flask_bootstrap import Bootstrap
 from configmodule import development_config
 from configmodule import production_config
+from search_form import SearchForm
 
 sys.path.insert(0, 'configmodule/')
 app = Flask(__name__)
+Bootstrap(app)
 
 
 @app.route('/')
-def root_method():
-    return 'Hello, Beautiful!'
+@app.route('/sentiment', methods=['POST', 'GET'])
+def my_sentiment():
+    thought_page = SearchForm()
+    if thought_page.validate_on_submit():
+        # Analzye code
+        flash('Analyzing your thoughts...')
+    return render_template('home.html', title='Understand your thoughts!', form=thought_page)
 
 
 if __name__ == '__main__':
-    app.config.from_object('production_config.ProductionConfig')
+    app.config.from_object('development_config.DevelopmentConfig')
     app.run()
