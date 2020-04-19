9d458583393d858ce409cf4ddb78333e2749c489
SiddharthaAnand <siddharthalibra13@gmail.com>
2020-04-19 20:24:47 +0530

    Update some text

--- search_form.py
+++ search_form.py
@@ -4,5 +4,6 @@ from wtforms.validators import DataRequired
 
 
 class SearchForm(FlaskForm):
-    search_word = StringField('What are you thinking today?', validators=[DataRequired()])
+    search_word = StringField('What are you thinking today?', validators=[DataRequired()],
+                              default="I am thinking about ...")
     submit = SubmitField('How am I doing?')
