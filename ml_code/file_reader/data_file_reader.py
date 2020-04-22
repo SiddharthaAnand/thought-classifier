f23f2113978037da0e71ecd3b0730a88026968bf
SiddharthaAnand <siddharthalibra13@gmail.com>
2020-04-22 22:09:59 +0530

    Add method which needs to overriden for every inhertance / file reader module

--- ml_code/file_reader/data_file_reader.py
+++ ml_code/file_reader/data_file_reader.py
@@ -1,5 +1,10 @@
 class BaseFileReader(object):
-    def __init__(self, filename=None, separator=None):
-        self.name = "base_file"
-        self.filename = filename
-        self.separator = separator
+    def __init__(self, reader="BaseFileReader"):
+        self.name = reader
+
+    def convert_to_df(self):
+        """
+        Needs to be overridden in every derived class.
+        :return: A dataframe which consists of columns with |data | polarity | values
+        """
+        pass
