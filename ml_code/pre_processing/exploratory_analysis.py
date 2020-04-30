ed747c1aa52246dced26deb0bbb5d7506e7911f2
SiddharthaAnand <siddharthalibra13@gmail.com>
2020-05-01 00:27:42 +0530

    Add method to see the most frequent words in the reviews

--- ml_code/pre_processing/exploratory_analysis.py
+++ ml_code/pre_processing/exploratory_analysis.py
@@ -11,6 +11,7 @@ Exploratory Analysis of the textual data available for sentiment analysis.
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 def read_and_reindex(filename=None, delimiter=None):
@@ -82,7 +83,40 @@ def text_cleaner(df=None):
     from ml_code.pre_processing import clean_text
     ctext = clean_text.CleanText()
     clean_review = ctext.fit_transform(df.text)
+    # Get some random sample to see how the processed text looks like!
     print(clean_review.sample(5))
+    return clean_review
+
+
+"""
+Check and replace empty rows with some text/null/avg values
+"""
+
+
+def fill_empty_reviews_with_no_text(cleaned_review=None, filler_text=None):
+    empty_review_rows = cleaned_review == ''
+    print('{} records have no words left after cleaning text'.format(cleaned_review[empty_review_rows].count()))
+    print('-' * 22)
+    cleaned_review.loc[empty_review_rows] = filler_text
+
+
+"""
+Get frequency of words in the reviews.
+"""
+
+
+def analyse_count_vectorizer_feature(cleaned_review=None):
+    import collections
+    import pandas as pd
+    cv = CountVectorizer()
+    bow = cv.fit_transform(cleaned_review)
+    word_freq = dict(zip(cv.get_feature_names(), np.asarray(bow.sum(axis=0)).ravel()))
+    word_counter = collections.Counter(word_freq)
+    word_counter_df = pd.DataFrame(word_counter.most_common(20), columns=['word', 'frequency'])
+
+    fig, ax = plt.subplots(figsize=(12, 10))
+    sns.barplot(x="word", y="frequency", data=word_counter_df, palette="PuBuGn_d", ax=ax)
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -108,4 +142,6 @@ if __name__ == '__main__':
     # 2. word_count_frame = clean_up_data(reindexed_data)
     # 3. visualize_word_count_and_polarity(word_count_frame)
     # show_distribution(word_count_frame, 'count_words')
-    text_cleaner(reindexed_data)
+    cleaned_review = text_cleaner(reindexed_data)
+    fill_empty_reviews_with_no_text(cleaned_review=cleaned_review, filler_text="[no_review_here]")
+    analyse_count_vectorizer_feature(cleaned_review)
