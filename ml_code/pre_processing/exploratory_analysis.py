6e0fd582b44e9539856768575aad520b9897a14d
SiddharthaAnand <siddharthalibra13@gmail.com>
2020-05-16 22:25:48 +0530

    Add one feature extraction method of count vectorizer

--- ml_code/pre_processing/exploratory_analysis.py
+++ ml_code/pre_processing/exploratory_analysis.py
@@ -251,3 +251,12 @@ if __name__ == '__main__':
 
     mnb = MultinomialNB()
     logreg = LogisticRegression()
+
+    countvect = CountVectorizer()
+    # MultinomialNB
+    best_mnb_countvect = grid_vect(mnb, parameters_mnb, X_train, X_test, parameters_text=parameters_vect, vect=countvect)
+    joblib.dump(best_mnb_countvect, '../output/best_mnb_countvect.pkl')
+    # LogisticRegression
+    best_logreg_countvect = grid_vect(logreg, parameters_logreg, X_train, X_test, parameters_text=parameters_vect, vect=countvect)
+    joblib.dump(best_logreg_countvect, '../output/best_logreg_countvect.pkl')
+
