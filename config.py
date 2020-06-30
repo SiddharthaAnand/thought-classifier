eb9d5fde81ebb672948c765e8af9bc48367c89d0
SiddharthaAnand <siddharthalibra13@gmail.com>
2020-06-30 22:59:38 +0530

    Add config for prod/stage/dev/testing

---
+++ config.py
@@ -0,0 +1,28 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = os.urandom(32)
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEBUG = True
+    DEVELOPMENT = True
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    DEVELOPMENT = True
+
+
+class TestingConfig(Config):
+    TESTING = True
