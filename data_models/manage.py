0c53e27a722597825ae8ef1a1a9bc4025f5f052f
SiddharthaAnand <siddharthalibra13@gmail.com>
2020-07-04 20:41:57 +0530

    Add manager for postgresql migrations

---
+++ data_models/manage.py
@@ -0,0 +1,21 @@
+# Migration
+import sys
+sys.path.insert(0, '/home/sid/github/thought-classifier/')
+
+import os
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+from main_app import app, db
+
+
+app.config.from_object(os.environ['APP_SETTINGS'])
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+
+if __name__ == '__main__':
+    manager.run()
