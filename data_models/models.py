4b2c39e98e14aa1ebf0914fa6d7317605c9fb4d5
SiddharthaAnand <siddharthalibra13@gmail.com>
2020-07-04 12:46:27 +0530

    Add table for storing the user input data

---
+++ data_models/models.py
@@ -0,0 +1,19 @@
+from main_app import db
+from sqlalchemy.dialects.postgresql import JSON
+
+
+class Result(db.Model):
+    __tablename__ = 'user_raw_data'
+
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String())
+    result = db.Column(JSON)
+    result_without_stopwords = db.Column(JSON)
+
+    def __init__(self, text=None, result=None, result_without_stopwords=None):
+        self.text = text
+        self.result = result
+        self.result_without_stopwords = result_without_stopwords
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
