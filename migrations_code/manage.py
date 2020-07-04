fd15b9477ed9d5383ca0a70f4b78fd94ddc39600
SiddharthaAnand <siddharthalibra13@gmail.com>
2020-07-04 22:06:37 +0530

    Set the path for the migrations folder

--- migrations_code/manage.py
+++ migrations_code/manage.py
@@ -8,10 +8,10 @@ from flask_migrate import Migrate, MigrateCommand
 
 from main_app import app, db
 
-# MIGRATION_DIR = os.path.join()
+MIGRATION_DIR = os.path.join('migrations_code', 'migrations')
 app.config.from_object(os.environ['APP_SETTINGS'])
 
-migrate = Migrate(app, db)
+migrate = Migrate(app, db, directory=MIGRATION_DIR)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
