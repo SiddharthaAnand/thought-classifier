1718610c303969ca549fa6c8176caf48db444f55
SiddharthaAnand <siddharthalibra13@gmail.com>
2020-07-07 11:59:04 +0530

    Refactor the main_app code and move out the processing to the worker

--- main_app.py
+++ main_app.py
@@ -9,7 +9,7 @@ from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 
-sys.path.insert(0, 'configmodule/')
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = urandom(32)
 app.config.from_object(environ['APP_SETTINGS'])
@@ -20,35 +20,6 @@ q = Queue(connection=conn)
 from data_models.models import Result
 
 Bootstrap(app)
-
-
-@app.route('/', methods=['POST', 'GET'])
-def index():
-    return render_template('home.html')
-
-
-@app.route('/sentiment', methods=['POST'])
-def analyze_sentiment():
-    if request.method == 'POST':
-        user_entered_text = json.loads(request.data.decode())['text']
-        print('Entering method: {}'.format(user_entered_text))
-        job = q.enqueue(
-            func=get_sentiment,
-            args=(user_entered_text,),
-            result_ttl=5000
-        )
-        return job.get_id()
-
-
-@app.route('/sentiment/<job_id>', methods=['GET'])
-def get_results(job_id):
-
-    job = Job.fetch(job_id, connection=conn)
-
-    if job.is_finished:
-        return str(job.result), 200
-    else:
-        return "Nay!", 202
 
 
 def get_sentiment(text):
@@ -72,6 +43,37 @@ def get_sentiment(text):
     except:
         results['errors'] = "Unable to add data to the database"
     return results
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    return render_template('home.html')
+
+
+@app.route('/sentiment', methods=['POST'])
+def analyze_sentiment():
+    if request.method == 'POST':
+
+
+        user_entered_text = request.form['text']
+        print('Entering method: {}'.format(user_entered_text))
+        job = q.enqueue_call(
+            func=get_sentiment,
+            args=(user_entered_text,),
+            result_ttl=5000
+        )
+        return job.get_id()
+
+
+@app.route('/sentiment/<job_id>', methods=['GET'])
+def get_results(job_id):
+
+    job = Job.fetch(job_id, connection=conn)
+
+    if job.is_finished:
+        return str(job.result), 200
+    else:
+        return "Nay!", 202
 
 
 if __name__ == '__main__':
