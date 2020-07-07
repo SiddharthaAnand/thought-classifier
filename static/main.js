9aecb583aab0f164cd33ebfa8d034eef80428178
SiddharthaAnand <siddharthalibra13@gmail.com>
2020-07-07 22:07:37 +0530

    Write a polling function which polls the redis queues for result completion

--- static/main.js
+++ static/main.js
@@ -4,7 +4,7 @@
 
   angular.module('ThoughtAnalyzerApp', [])
 
-  .controller('ThoughtAnalyzerController', ['$scope', '$log', '$http',
+  .controller('ThoughtAnalyzerController', ['$scope', '$log', '$http', '$timeout',
       function($scope, $log, $http, $timeout) {
 
         $scope.getResults = function() {
@@ -16,13 +16,38 @@
             $log.log(userInput);
             // fire the API request
             $http.post('/sentiment', {"text": userInput}).
-              success(function(results) {
-                $log.log(results);
+              success(function(jobId) {
+                $log.log(jobId);
+                getSentiment(jobId);
               }).
               error(function(error) {
                 $log.log(error);
-              })
+              });
         };
+
+      function getSentiment(jobID) {
+
+      var timeout = "";
+
+      var poller = function() {
+        // fire another request
+        $http.get('/sentiment/'+jobID).
+          success(function(data, status, headers, config) {
+            if(status === 202) {
+              $log.log(data, status);
+            } else if (status === 200){
+              $log.log(data);
+              $scope.results = data;
+              $timeout.cancel(timeout);
+              return false;
+            }
+            // continue to call the poller() function every 2 seconds
+            // until the timeout is cancelled
+            timeout = $timeout(poller, 2000);
+          });
+      };
+      poller();
+    }
       }
       ]);
 
