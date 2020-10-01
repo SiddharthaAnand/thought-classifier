71a6fd8ceb7b0aa2b57bfb279a245fd1121a4b81
SiddharthaAnand <siddharthalibra13@gmail.com>
2020-10-01 19:31:59 +0530

    Delete the model int he finally block to make sure it does not stay in memory once used | Avoid memory error on heroku

--- ml_code/class_predictor.py
+++ ml_code/class_predictor.py
@@ -1,9 +1,11 @@
 import sys
+import pandas as pd
 sys.path.insert(0, '/home/sid/github/thought-classifier/')
 from sklearn.externals import joblib
 from ml_code.pre_processing import clean_text
 from ml_code.pre_processing import text_count
 loaded_model = joblib.load('ml_code/output/best_logreg_final_model.pkl')
+
 
 def read_model_and_predict(text):
     """
@@ -11,18 +13,25 @@ def read_model_and_predict(text):
     Uses it to predict the text polarity.
     :return: None
     """
-    import pandas as pd
-    new_positive_tweets = pd.Series(text)
-    tc = text_count.TextCount()
-    ct = clean_text.CleanText()
-    df_counts_pos = tc.transform(new_positive_tweets)
-    df_clean_pos = ct.transform(new_positive_tweets)
-    df_model_pos = df_counts_pos
-    df_model_pos['clean_text'] = df_clean_pos
-    print("Predicting from the loaded pickled model...")
-    # print(loaded_model.predict(df_model_pos).tolist())
-    del loaded_model
-    return loaded_model.predict(df_model_pos).tolist()
+    try:
+        global loaded_model
+        new_positive_tweets = pd.Series(text)
+        tc = text_count.TextCount()
+        ct = clean_text.CleanText()
+        df_counts_pos = tc.transform(new_positive_tweets)
+        df_clean_pos = ct.transform(new_positive_tweets)
+        df_model_pos = df_counts_pos
+        df_model_pos['clean_text'] = df_clean_pos
+        print("Predicting from the loaded pickled model...")
+        return loaded_model.predict(df_model_pos).tolist()
+    except RuntimeError as e:
+        pass
+    finally:
+        #################################################################
+        # This statement required to try using less memory while running#
+        #################################################################
+        del loaded_model
+
 
 if __name__ == '__main__':
     text = ["TThank you @VirginAmerica for you amazing customer support team on Tuesday 11/28 at @EWRairport and returning my lost bag in less than 24h! #efficiencyiskey #virginamerica",
